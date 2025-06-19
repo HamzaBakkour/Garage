67cb9b8d642ae568e82053489a8f21ec76839149
HamzaBakkour <alexander.x4.hb@outlook.com>
2025-06-19 10:33:59 +0200

    Testing DI

--- Garage/Program.cs
+++ Garage/Program.cs
@@ -12,15 +12,11 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        IConfiguration config = new ConfigurationBuilder()
-                                .SetBasePath(Environment.CurrentDirectory)
-                                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                                .Build();
+
 
         var host = Host.CreateDefaultBuilder(args)
                         .ConfigureServices(services =>
                         {
-                            services.AddSingleton<IConfiguration>(config);
                             services.AddSingleton<IUI, ConsoleUI>();
                             services.AddSingleton<ICreateVehicle, CreateVehicle>();
                             
