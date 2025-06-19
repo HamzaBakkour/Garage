3627f337c331b0492eec88f971cbe9d232eb85f7
HamzaBakkour <alexander.x4.hb@outlook.com>
2025-06-19 10:30:55 +0200

    Testing DI

--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Garage.Abstraction;
 using Garage.UI;
 using Garage.Vehicles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 namespace Garage;
 
@@ -8,26 +12,63 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        IConfiguration config = new ConfigurationBuilder()
+                                .SetBasePath(Environment.CurrentDirectory)
+                                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                .Build();
 
-        var initialVehicles = new List<Vehicle>
-        {
-            new Car("abc1", "Red", 1, 122),
-            new Motorcycle("abc2", "Blue", 2, 50),
-            new Bus("abc3", "Green", 4, 332)
+        var host = Host.CreateDefaultBuilder(args)
+                        .ConfigureServices(services =>
+                        {
+                            services.AddSingleton<IConfiguration>(config);
+                            services.AddSingleton<IUI, ConsoleUI>();
+                            services.AddSingleton<ICreateVehicle, CreateVehicle>();
 
-        };
+                            services.AddSingleton<List<Vehicle>>(new List<Vehicle>
+                            {
+                                new Car("abc1", "Red", 1, 122),
+                                new Motorcycle("abc2", "Blue", 2, 50),
+                                new Bus("abc3", "Green", 4, 332)
+                            });
 
-        ICreateVehicle createVehicle = new CreateVehicle();
+                            // Register GarageHandler
+                            services.AddSingleton<IGarageHandler<Vehicle>>(provider =>
+                            {
+                                var ui = provider.GetRequiredService<IUI>();
+                                var vehicles = provider.GetRequiredService<List<Vehicle>>();
+                                return new GarageHandler<Vehicle>(15, vehicles, ui);
+                            });
+
+                            services.AddSingleton<Main>();
+
+                        })
+                        .UseConsoleLifetime()
+                        .Build();
+
+
+        host.Services.GetRequiredService<Main>().Run();
+
+        Console.WriteLine("Garage app ended.");
+
+        //var initialVehicles = new List<Vehicle>
+        //{
+        //    new Car("abc1", "Red", 1, 122),
+        //    new Motorcycle("abc2", "Blue", 2, 50),
+        //    new Bus("abc3", "Green", 4, 332)
+
+        //};
+
+        //ICreateVehicle createVehicle = new CreateVehicle();
 
 
 
-        IUI ui = new ConsoleUI();
+        //IUI ui = new ConsoleUI();
 
-        IGarageHandler<Vehicle> garageHandler = new GarageHandler<Vehicle>(15, initialVehicles, ui);
+        //IGarageHandler<Vehicle> garageHandler = new GarageHandler<Vehicle>(15, initialVehicles, ui);
 
 
-        Main main = new Main(garageHandler, createVehicle, ui);
+        //Main main = new Main(garageHandler, createVehicle, ui);
 
-        main.Run();
+        //main.Run();
     }
 }
